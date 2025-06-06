06d536bd6e0311932a38f12e96a28c612c4671d1
Harsh1s <thisishraj@gmail.com>
2025-06-06 16:17:09 +0530

    Add datenlord yearlong experiment case 01

---
+++ src/harsh1s_yearlong_cases.rs
@@ -0,0 +1,13 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct YearlongCase {
+    pub date: &'static str,
+    pub seed: u32,
+}
+
+pub const CASES: &[YearlongCase] = &[
+    YearlongCase { date: "2025-06-06", seed: 133816 },
+];
+
+pub const fn case_count() -> usize {
+    CASES.len()
+}
