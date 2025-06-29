bb382851e0097ab967d6402840326bf5b473f517
Harsh1s <thisishraj@gmail.com>
2025-06-29 23:35:55 +0530

    Add datenlord yearlong experiment case 04

--- src/harsh1s_yearlong_cases.rs
+++ src/harsh1s_yearlong_cases.rs
@@ -8,6 +8,7 @@ pub const CASES: &[YearlongCase] = &[
     YearlongCase { date: "2025-06-06", seed: 133816 },
     YearlongCase { date: "2025-06-14", seed: 320767 },
     YearlongCase { date: "2025-06-20", seed: 587777 },
+    YearlongCase { date: "2025-06-29", seed: 803637 },
 ];
 
 pub const fn case_count() -> usize {
